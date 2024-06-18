da5f14c7acb0938d8995cd1e0e3b01ed44c2adea
Mattia <82611659+Xhst@users.noreply.github.com>
2024-06-18 14:20:04 +0200

    feat: ml-gol

---
+++ openmp/include/ml-gol.h
@@ -0,0 +1,8 @@
+#ifndef __ML_GOL_H
+#define __ML_GOL_H
+
+#include <stdint.h>
+
+void start_game(uint32_t grid_size, uint32_t num_steps, uint32_t seed);
+
+#endif
---
+++ openmp/src/ml-gol.c
@@ -0,0 +1,11 @@
+#include "ml-gol.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void start_game(uint32_t grid_size, uint32_t num_steps, uint32_t seed)
+{
+    srand(seed);
+
+    printf("%d\n", rand());
+}
