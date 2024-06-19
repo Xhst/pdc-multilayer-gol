5a4ef665b85e758ee92059d0590a37cf7ceb9db4
Mattia <82611659+Xhst@users.noreply.github.com>
2024-06-19 09:49:23 +0200

    feat: calculate combined

--- openmp/src/ml-gol.c
+++ openmp/src/ml-gol.c
@@ -5,6 +5,24 @@
 
 #define DEFAULT_DENSITY 0.3
 
+void start_game(const uint32_t grid_size, const uint32_t num_layers, const uint32_t num_steps, const uint32_t seed) {
+    srand(seed);
+
+    ml_gol_t* ml_gol = (ml_gol_t*) malloc(sizeof(ml_gol_t));
+    init_ml_gol(ml_gol, grid_size, num_layers, seed);
+
+    for (uint32_t s = 0; s < num_steps; s++) {
+        for (uint32_t i = 0; i < num_layers; i++) {
+            fill_ghost_cells(&ml_gol->layers[i]);
+            step(&ml_gol->layers[i]);
+            swap_grids(&ml_gol->layers[i]);
+        }
+        calculate_combined(ml_gol);
+    }
+
+    free_ml_gol(ml_gol);
+}
+
 void init_ml_gol(ml_gol_t* ml_gol, const uint32_t grid_size, const uint32_t num_layers, const uint32_t seed) {
     ml_gol->num_layers = num_layers;
     ml_gol->layers = (gol_t*) malloc(num_layers * sizeof(gol_t));
@@ -22,21 +40,17 @@ void init_ml_gol(ml_gol_t* ml_gol, const uint32_t grid_size, const uint32_t num_
     ml_gol->dependent = (cell_t*) malloc(size * sizeof(cell_t));
 }
 
-void start_game(const uint32_t grid_size, const uint32_t num_layers, const uint32_t num_steps, const uint32_t seed) {
-    srand(seed);
+void calculate_combined(const ml_gol_t* ml_gol) {
+    for (uint32_t i = 0; i < ml_gol->grid_size; i++) {
+        for (uint32_t j = 0; j < ml_gol->grid_size; j++) {
+            uint32_t idx = i * ml_gol->grid_size + j;
 
-    ml_gol_t* ml_gol = (ml_gol_t*) malloc(sizeof(ml_gol_t));
-    init_ml_gol(ml_gol, grid_size, num_layers, seed);
-
-    for (uint32_t s = 0; s < num_steps; s++) {
-        for (uint32_t i = 0; i < num_layers; i++) {
-            fill_ghost_cells(&ml_gol->layers[i]);
-            step(&ml_gol->layers[i]);
-            swap_grids(&ml_gol->layers[i]);
+            ml_gol->dependent[idx] = 0;
+            for (uint32_t k = 0; k < ml_gol->num_layers; k++) {
+                 ml_gol->dependent[idx] += ml_gol->layers[k].current[idx];
+            }
         }
     }
-
-    free_ml_gol(ml_gol);
 }
 
 void free_ml_gol(ml_gol_t* ml_gol) {
