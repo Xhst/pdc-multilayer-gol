22b4e49444aa181821b4ea59917cb5e574b6aafd
Mattia <82611659+Xhst@users.noreply.github.com>
2024-06-19 13:57:27 +0200

    feat: write png file

---
+++ openmp/src/image.c
@@ -0,0 +1,44 @@
+#include "image.h"
+
+#include <png.h>
+#include <stdlib.h>
+
+void write_png_file(const char* filename, uint32_t width, uint32_t height, const uint8_t* buffer) {
+    FILE *fp = fopen(filename, "wb");
+    if(!fp) abort();
+
+    png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+    if (!png) abort();
+
+    png_infop info = png_create_info_struct(png);
+    if (!info) abort();
+
+    if (setjmp(png_jmpbuf(png))) abort();
+
+    png_init_io(png, fp);
+
+    // Output is 8-bit depth, RGBA format.
+    png_set_IHDR(
+        png,
+        info,
+        width, height,
+        8,
+        PNG_COLOR_TYPE_RGBA,
+        PNG_INTERLACE_NONE,
+        PNG_COMPRESSION_TYPE_DEFAULT,
+        PNG_FILTER_TYPE_DEFAULT
+    );
+    png_write_info(png, info);
+
+    png_bytep rows[height];
+    for(uint32_t y = 0; y < height; y++) {
+        rows[y] = &buffer[y * width * 4];
+    }
+
+    png_write_image(png, rows);
+    png_write_end(png, NULL);
+
+    fclose(fp);
+
+    png_destroy_write_struct(&png, &info);
+}
