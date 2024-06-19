66dcb0699c12d65a281639dc940ffb165ef55334
Mattia <82611659+Xhst@users.noreply.github.com>
2024-06-19 15:40:19 +0200

    feat: color hsv

--- openmp/include/color.h
+++ openmp/include/color.h
@@ -1,10 +1,33 @@
 #ifndef __COLOR_H
 #define __COLOR_H
 
+#include <stdint.h>
+
+typedef struct {
+    // Hue (0-360)
+    double h; 
+
+    // Saturation (0-1)
+    double s; 
+
+    // Value (brightness, 0-1)
+    double v; 
+} color_hsv_t;
+
 typedef struct {
     uint8_t r;
     uint8_t g;
     uint8_t b;
 } color_t;
+
+#define BLACK   ((color_t){0, 0, 0})
+#define RED     ((color_t){255, 0, 0})
+#define GREEN   ((color_t){0, 255, 0})
+#define BLUE    ((color_t){0, 0, 255})
+#define WHITE   ((color_t){255, 255, 255})
+
+color_t add_colors(const color_t c1, const color_t c2);
+
+color_t hsv_to_rgb(color_hsv_t hsv);
 
 #endif
